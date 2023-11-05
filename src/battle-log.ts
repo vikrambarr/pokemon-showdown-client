99c8b3ddba60fec08707082b711876df309ece91
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-11-05 21:17:06 +0000

    add back daily cachebuster

--- src/battle-log.ts
+++ src/battle-log.ts
@@ -1173,7 +1173,7 @@ export class BattleLog {
 		buf += '<div class="battle-log battle-log-inline"><div class="inner">' + battle.scene.log.elem.innerHTML + '</div></div>\n';
 		buf += '</div>\n';
 		buf += '<script>\n';
-		buf += `let daily = Math.floor(Date.now()/1000/60/60/24);document.write('<script src="http://play.pokeathlon.com/js/replay-embed.js"></'+'script>');\n`;
+		buf += `let daily = Math.floor(Date.now()/1000/60/60/24);document.write('<script src="http://play.pokeathlon.com/js/replay-embed.js?version'+daily+'"></'+'script>');\n`;
 		buf += '</script>\n';
 		return buf;
 	}
