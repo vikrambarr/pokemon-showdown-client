d7b84585bb6a72104c0e43ac31077450333bb282
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-11-06 00:14:45 +0000

    remove interstice by default

--- js/client-topbar.js
+++ js/client-topbar.js
@@ -16,6 +16,7 @@
 			// Set some default prefs
 			if (Storage.prefs('bwgfx') === undefined) Storage.prefs('bwgfx', true);
 			if (Storage.prefs('theme') === undefined) Storage.prefs('theme', 'dark');
+			if (Storage.prefs('hideinterstice') === undefined) Storage.prefs('hideinterstice', true);
 			
 			// April Fool's 2016 - Digimon Showdown
 			// this.$el.html('<img class="logo" src="' + Dex.resourcePrefix + 'sprites/afd/digimonshowdown.png" alt="Digimon Showdown! (beta)" width="146" height="44" /><div class="maintabbarbottom"></div><div class="tabbar maintabbar"><div class="inner"></div></div><div class="userbar"></div>');
