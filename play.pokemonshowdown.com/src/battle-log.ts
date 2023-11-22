e473e9a8a7e1bb26006c62c61dd59dd206a9509e
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-11-22 13:18:04 +0000

    swap victini/jirachi + custom name colors

--- play.pokemonshowdown.com/src/battle-log.ts
+++ play.pokemonshowdown.com/src/battle-log.ts
@@ -557,7 +557,8 @@ export class BattleLog {
 		if (this.colorCache[name]) return this.colorCache[name];
 		let hash;
 		if (Config.customcolors[name]) {
-			hash = MD5(Config.customcolors[name]);
+			this.colorCache[name] = Config.customcolors[name];
+			return this.colorCache[name];
 		} else {
 			hash = MD5(name);
 		}
