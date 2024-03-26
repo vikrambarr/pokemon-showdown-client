d7c9927f230a8dd882b5074de269a380019346ff
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-03-26 17:10:59 +0000

    add support for sunflora

--- play.pokemonshowdown.com/src/battle-dex-data.ts
+++ play.pokemonshowdown.com/src/battle-dex-data.ts
@@ -1543,7 +1543,7 @@ class Species implements Effect {
 		this.tier = data.tier || '';
 
 		this.isTotem = false;
-		this.isMega = !!(this.forme && ['-mega', '-megax', '-megay'].includes(this.formeid));
+		this.isMega = !!(this.forme && ['-mega', '-megax', '-megay', '-megam', '-megaf'].includes(this.formeid));
 		this.isPrimal = !!(this.forme && this.formeid === '-primal');
 		this.canGigantamax = !!data.canGigantamax;
 		this.cannotDynamax = !!data.cannotDynamax;
